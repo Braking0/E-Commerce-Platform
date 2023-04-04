96b687d96448e68a8b52ce6a5993610d878ceb41
Braking0 <128966084+Braking0@users.noreply.github.com>
2023-04-04 13:33:48 +0100

    Add files via upload

---
+++ E-commerce-Platform/script.js
@@ -0,0 +1,16 @@
+'use stict';
+const modal = document.querySelector('[data-modal]');
+const modalCloseBtn = document.querySelector('[data-modal-close]');
+const modalCloseOverlay = document.querySelector('[data-modal-overlay]');
+
+const modalCloseFunc = function() { modal.classList.add('closed')}
+
+modalCloseOverlay.addEventListener('click', modalCloseFunc);
+modalCloseBtn.addEventListener('click', modalCloseFunc);
+
+const notificationToast = document.querySelector('[data-toast]');
+const toastCloseBtn = document.querySelector('[data-toast-close]');
+
+toastCloseBtn.addEventListener('click', function() {
+    notificationToast.classList.add('closed');
+})
